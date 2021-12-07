0befad023c8e0e91ccd8d2af52a26b9f9d8daa52
tmdd89 <taimoor.m89@gmail.com>
2021-12-07 03:50:01 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="">
+      
+     <h1>Spotify App Ting!</h1>
+     <main>
+       {/* sidebar */}
+       {/* center */}
+     </main>
+
+     <div>{/* player */}</div> 
+    </div>
+  )
+}
