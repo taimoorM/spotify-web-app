f733591acf7582d8c9e6b92eed3a3165bbcac2af
tmdd89 <taimoor.m89@gmail.com>
2021-12-07 16:36:29 -0500

    added basic ui for sidebar navigation

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
   return (
     <div className="">
-      
-     <h1>Spotify App Ting!</h1>
-     <main>
-       {/* sidebar */}
-       {/* center */}
-     </main>
+      <main>
+        <Sidebar />
+        {/* center */}
+      </main>
 
-     <div>{/* player */}</div> 
+      <div>{/* player */}</div>
     </div>
-  )
+  );
 }
